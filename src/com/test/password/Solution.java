d1b84057ea0c153de2b4abf229e61599aac0f286
Sameer Bhalerao <sbhalera@adobe.com>
2022-06-10 18:54:41 +0530

    Using priority queuq

--- src/com/test/password/Solution.java
+++ src/com/test/password/Solution.java
@@ -2,8 +2,9 @@ package com.test.password;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.PriorityQueue;
 
 class Result {
 
@@ -18,52 +19,69 @@ class Result {
 
     public static String passwordCracker(List<String> passwords, String loginAttempt) {
         // Write your code here
-        List<String> myList = new ArrayList<>();
-        if (loginAttempt.length() > 0) {
-            myList = findPassword(passwords, loginAttempt, myList);
-        }
+        List<String> myList =
+                myList = findPassword(passwords, loginAttempt);
+
         if (myList.size() > 0) {
             String str = myList.stream().reduce("", (s, s2) -> {
                 return (s + " " + s2);
             }).substring(1);
             if (str.contains(" ...")) {
                 str = str.substring(0, str.indexOf(" ..."));
+            } else {
+                return "WRONG PASSWORD";
             }
             return str;
         }
         return "WRONG PASSWORD";
     }
 
-    public static List<String> findPassword(List<String> passwords, String loginAttempt, List<String> list) {
-        if (loginAttempt.length() == 0) {
+    public static List<String> findPassword(List<String> passwords, String loginAttempt) {
+        List<String> list = new ArrayList<>();
+        if (loginAttempt.length() <= 0) {
             list.add("...");
             return list;
         } else {
             //if a prefix matches with one of passwords
-            List<String> result = passwords.stream().filter(s -> loginAttempt.startsWith(s)).collect(Collectors.toList());
-            if (result.size() > 0)
-                for (int i = 0; i < result.size(); i++) {
-                    int size = list.size();
-                    if (size > 0 && list.get(size - 1).equals("...")) {
-                        list.remove(size - 1);
-                        return list;
+            PriorityQueue<String> passwordQueue = new PriorityQueue<>(3 * passwords.size(), new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    if (o1.length() > o2.length()) {
+                        return -1;
+                    } else if (o1.length() == o2.length()) {
+                        return 0;
+                    } else {
+                        return 1;
                     }
-                    list.add(result.get(i));
-                    findPassword(passwords, loginAttempt.substring(result.get(i).length()), list);
                 }
-            else {
-                int size = list.size();
-                list.remove(size - 1);
+            });
+            for (int i = 0; i < passwords.size(); i++) {
+                if (loginAttempt.startsWith(passwords.get(i))) {
+                    passwordQueue.add(passwords.get(i));
+                }
             }
+            //List<String> result = passwords.stream().filter(s -> loginAttempt.startsWith(s)).collect(Collectors.toList());
+            if (passwordQueue.peek() != null) {
+
+                int size = list.size();
+                if (size > 0 && list.get(size - 1).equals("...")) {
+                    //already found. return;
+                    return list;
+                } else {
+                    String password = passwordQueue.poll();
+                    list.add(password);
+                    list.addAll(findPassword(passwords, loginAttempt.substring(password.length())));
+                }
+            }
+            return list;
         }
-        return list;
     }
 }
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        List<String> passwords = List.of("ab", "abcd", "cd");
-        String result = Result.passwordCracker(passwords, "abcd");
+        List<String> passwords = List.of("a" ,"b" ,"ab");
+        String result = Result.passwordCracker(passwords, "abbababbaabababbababaabbbbaabababbaabaaaabaabbbabababbbaababbabababbabbbbbababaabaabaabbabbabaababbababbabbbbaababaaabbababaaababababbababbaabbabaaabbabaabbababaaaaaaabbaaababababaababaabbabaaabbababbababaaababaababaababbababaabbaabaabaababaabbabbabaabbaabaabbababaacbbaaabbababbaaababaaaabbabababbbbababababbaabbaaababababbbabbaabbaabababaabaaababaaaababbbaabababababbabababbaaababbaabaabaababababbaaabababaabbabaababbbababbaabbbababaababababaababbbbabaaabababaabbabaabaabbbaabababaababbaaabbabaababbbabaabbaababaaabbababbabaaabababababaaababbabababbababaaabababababbabbabaabaabbabaabaabaabababbbabbabababaaabbbaabaababbbaaaabaabababaaaabbababbaabababbbababababababaababaaabbbabababbaaaababbbabbaababababababababababaabbaaabbbabbababaaaabaaabbabaaaababbaabaaaabbababbabbabbbabbabbababaabababbaabbababbabbabbaabaabbabbbabababaababbaababbaababaabbabbbbbababbabbabaabaababbabbaaababbbabababbababaaabaabbababababbababbababaababaababababbbabaaaababababaaabbabbbabababbabbbabababbaabbabbabbabababaaabbabababababbaabababababaabaababbbabaababbbabaabbbbababbbabaabbabaabaabbbabbbbababbabbabaaabaababbbabababaaabbaabaaabbbbaaabbbabaaabbbbbbabababbaaaabbabababaabbabbbaaabbbababbaababbbababaaababbababbabababaaabaabaabaaaababbbabaabbabaaabbbabbabababababaaaabbaabababbbabababbbaabbaababbaaababbaabaaaabbabababaaabaababbbaabaabaabaaababaababaababababbbbabbbbababbaababaaabaababababbbabbabababababaaabababaabbbabbbabababaabbabababbbaabababbaaababbaabbbabababbabbababbababaababaabaaaabbaababababbababbbababbbbabbbaababaaaababbabbabaabbbabbaabababababbbabababbbbabaabababbbabaaaababababbbabaaabbaabababbbbaabaababbababaaababbabaabaabbabababababababbababababaababaaababaabababbababababaabbabaabaaabbbabbabaabaaabababaabaabababbbbbbbbababababaabbabbabbaababbaabbababaabbbabababaabaabbaaabbabbaaabbbbabaabbabbbbabbabaabbaabaaabbbababbbbbbababaababbaabababbababababbaaabaabbabaaaaaaabbababaabaabbababbabbabbaaabbabbabaabaaababaabbbaabbabbbabababaabaaaabbbabaaaababaabaababaaababbaabaabbbaababababbbbaaabababbbaaabaaaba");
         System.out.println(result);
     }
 }
